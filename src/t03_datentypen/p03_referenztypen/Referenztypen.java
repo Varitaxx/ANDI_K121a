96627a031ba395ff82dfaf639e15aefd64148b01
Salah Lejmi <lejmi@gmx.de>
2022-09-13 10:09:50 +0200

    reorganized

---
+++ src/t03_datentypen/p03_referenztypen/Referenztypen.java
@@ -0,0 +1,10 @@
+package t03_datentypen.p03_referenztypen;
+
+public class Referenztypen {
+
+  public static void main( String[] args ) {
+    System.out.println("============================================================");
+    System.out.println("======================= end of main() ======================");
+  }
+  
+}
