8e84da334d64923023dc5cb0f6a0643ae6640848
Salah Lejmi <lejmi@gmx.de>
2022-09-19 09:18:32 +0200

    Typumwandlung: implizit/automatisch, explizit/casting

--- src/t04_t05_operatoren/S07_Prioritäten.java
+++ src/t04_t05_operatoren/S07_Prioritäten.java
@@ -1,5 +1,9 @@
 package t04_t05_operatoren;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 public class S07_Prioritäten {
   public static void main(String[] args) {
     // Unäre Operatoren
