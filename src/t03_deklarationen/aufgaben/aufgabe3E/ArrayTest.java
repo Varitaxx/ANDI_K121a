ff897a5a6eb1c9cad174bdb490e79f4876d4e4a6
Salah Lejmi <lejmi@gmx.de>
2022-09-29 10:22:57 +0200

    Aufgabe 3E - Summe und PlusMinus - lösung 1 .. 4

--- src/t03_deklarationen/aufgaben/aufgabe3E/ArrayTest.java
+++ src/t03_deklarationen/aufgaben/aufgabe3E/ArrayTest.java
@@ -3,17 +3,49 @@ package t03_deklarationen.aufgaben.aufgabe3E;
 public class ArrayTest {
   
   static double[] zahlen = { 4.5, 78, 23.9, 12.7, 88 };
+  
   public static void main( String[] args ) {
     System.out.println("============================================================");
-    //lösung1();
-    lösung2();
+    lösung1();
+    //lösung2();
+    //lösung3();
+    //lösung4();
     System.out.println("======================= end of main() ======================");
+  }
+  
+  private static void lösung4() {
+    double summe = 0;
+    double plusMinus = 0;
+    
+    for( int i = 0 ; i < zahlen.length ; i++ ) { // Standard for-Schleife
+      plusMinus = plusMinus + (i % 2 == 0 ? +zahlen[i] : -zahlen[i]);
+      summe = summe + zahlen[i];
+    }
+    
+    System.out.format("Reguläre Summe  : %10.2f %n", summe);
+    System.out.format("Plus-Minus-Summe: %10.2f %n", plusMinus);
+  }
+  
+  private static void lösung3() {
+    double summe = 0;
+    double plusMinus = 0;
+    int i = 0;
+    for( double zahl : zahlen ) { // Erweiterte for-Schleife
+      if (i % 2 == 0) // wenn i gerade Zahl ist
+        plusMinus = plusMinus + zahl;
+      else            // i ist ungerade
+        plusMinus = plusMinus - zahl;
+      summe = summe + zahl;
+      i++;
+    }
+    System.out.format("Reguläre Summe  : %10.2f %n", summe);
+    System.out.format("Plus-Minus-Summe: %10.2f %n", plusMinus);
   }
   
   private static void lösung2() {
     double summe = 0;
-  
     double plusMinus = 0;
+   
     for( int i = 0 ; i < zahlen.length ; i++ ) { // Standard for-Schleife
       if (i % 2 == 0) // wenn i gerade Zahl ist
         plusMinus = plusMinus + zahlen[i];
@@ -21,18 +53,20 @@ public class ArrayTest {
         plusMinus = plusMinus - zahlen[i];
       summe = summe + zahlen[i];
     }
-    System.out.format("Reguläre Summe  : %10.2f %n",summe);
-    System.out.format("Plus-Minus-Summe: %10.2f %n",plusMinus);
+    
+    System.out.format("Reguläre Summe  : %10.2f %n", summe);
+    System.out.format("Plus-Minus-Summe: %10.2f %n", plusMinus);
   }
   
   private static void lösung1() {
     // =============================================================================
     double summe = 0;
-    for ( double zahl : zahlen ) { // Erweiterte for-Schleife (enhanced for loop = foreach)
+    for( double zahl : zahlen ) { // Erweiterte for-Schleife (enhanced for loop = foreach)
       summe = summe + zahl; // summe += zahl;
     }
     //System.out.println("Summe: " + summe);
-    System.out.format("Summe: %10.2f %n", summe);
+    System.out.format("Reguläre Summe  : %10.2f %n", summe);
+  
     // =============================================================================
     double plusMinus = 0;
     for( int i = 0 ; i < zahlen.length ; i++ ) {
@@ -43,7 +77,7 @@ public class ArrayTest {
     }
     // =============================================================================
     //System.out.println("Plus-Minus-Summe: " + plusMinus);
-    System.out.format("Plus-Minus-Summe: %10.2f %n",plusMinus);
+    System.out.format("Plus-Minus-Summe: %10.2f %n", plusMinus);
     // Zur Kontrolle
     //System.out.println(4.5 - 78 + 23.9 - 12.7 + 88 ); // 25.7 (OK)
   }
