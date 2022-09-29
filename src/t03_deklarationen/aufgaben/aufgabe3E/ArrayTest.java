997a3d208eece0d1f2bd71e53f5ba335fd79c197
Salah Lejmi <lejmi@gmx.de>
2022-09-29 09:30:52 +0200

    Aufgabe 3E - Summe und PlusMinus - update

--- src/t03_deklarationen/aufgaben/aufgabe3E/ArrayTest.java
+++ src/t03_deklarationen/aufgaben/aufgabe3E/ArrayTest.java
@@ -14,7 +14,7 @@ public class ArrayTest {
     double summe = 0;
   
     double plusMinus = 0;
-    for( int i = 0 ; i < zahlen.length ; i++ ) {
+    for( int i = 0 ; i < zahlen.length ; i++ ) { // Standard for-Schleife
       if (i % 2 == 0) // wenn i gerade Zahl ist
         plusMinus = plusMinus + zahlen[i];
       else            // i ist ungerade
@@ -26,13 +26,14 @@ public class ArrayTest {
   }
   
   private static void lösung1() {
+    // =============================================================================
     double summe = 0;
-    for ( double zahl : zahlen ) { // foreach-Syntax (enhanced for loop)
+    for ( double zahl : zahlen ) { // Erweiterte for-Schleife (enhanced for loop = foreach)
       summe = summe + zahl; // summe += zahl;
     }
     //System.out.println("Summe: " + summe);
-    System.out.format("Summe: %10.2f %n",summe);
-    
+    System.out.format("Summe: %10.2f %n", summe);
+    // =============================================================================
     double plusMinus = 0;
     for( int i = 0 ; i < zahlen.length ; i++ ) {
       if (i % 2 == 0) // wenn i gerade Zahl ist
@@ -40,6 +41,7 @@ public class ArrayTest {
       else            // i ist ungerade
         plusMinus = plusMinus - zahlen[i];
     }
+    // =============================================================================
     //System.out.println("Plus-Minus-Summe: " + plusMinus);
     System.out.format("Plus-Minus-Summe: %10.2f %n",plusMinus);
     // Zur Kontrolle
